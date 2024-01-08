b45f3113247dc91e5f541cc2d1e832c7b543d85d
Natalie Harrison <harrisonNatalie04@outlook.com>
2024-01-08 10:01:08 -0500

    City comp and route
    -Create a /cities route to display list of heroes
    -Cities.component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     MessagesComponent,
     DashboardComponent,
     HeroesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
