da1ed5af0f41f1c479d31499d3f6de6102cd02d7
Natalie Harrison <harrisonNatalie04@outlook.com>
2024-01-09 13:35:23 -0500

    OnClick event City button
    -City on click button event displays "heroes in {{city.name}}

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CITIES } from '../mock.cities';
 import { NgFor } from '@angular/common';
+import { City } from '../city';
+import { HeroesComponent } from '../heroes/heroes.component';
 
 @Component({
   selector: 'app-cities',
@@ -10,4 +12,8 @@ import { NgFor } from '@angular/common';
 export class CitiesComponent {
   cities = CITIES;
 
+  selectedCity?: City;
+  onSelect(city:City): void{
+    this.selectedCity = city; 
+  }
 }
