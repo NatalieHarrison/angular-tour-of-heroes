2c4b1d79caead7a576db96b1db2eb27f2dc0b205
Natalie Harrison <harrisonNatalie04@outlook.com>
2024-01-09 11:42:20 -0500

    display list of cities
    List of cities displayed from mock cities list

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CITIES } from '../mock.cities';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
-  styleUrl: './cities.component.css'
+  styleUrl: './cities.component.css',
 })
 export class CitiesComponent {
+  cities = CITIES;
 
 }
