b71bd58b7fa83c7e999c96811ce850463d317ab6
Natalie Harrison <harrisonNatalie04@outlook.com>
2024-01-12 08:56:57 -0500

    Revert "Revert "edit city - drop down""
    This reverts commit f5a4d75c0b63ca9ff76b15d6a79978a63a24b462.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,36 +1,49 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {NgIf, UpperCasePipe} from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from '../hero.service';
 import { Observable } from 'rxjs';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+
+import { City } from '../city';
+import { CityService } from '../city.service';
 
 @Component({
   standalone: true, 
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css',
-  imports: [FormsModule, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgIf, UpperCasePipe, MatAutocompleteModule,MatInputModule, ReactiveFormsModule ],
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
+  cities: City[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
+    private cityService: CityService,
     private location: Location
   ) {}
 
   ngOnInit(): void {
     this.getHero();
+    this.getCities();
   }
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  getCities(): void{
+    this.cityService.getCities()
+    .subscribe(cities => this.cities = cities);
   }
 
   goBack(): void {
@@ -44,4 +57,7 @@ export class HeroDetailComponent implements OnInit {
       .subscribe( ()=> this.goBack());
     }
   }
+
+  myControl = new FormControl('');
+  options:  City[] = [];
 }
