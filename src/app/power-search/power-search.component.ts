0dc0abff682a8fe7d7af8803c082c52e16ecbfbd
Natalie Harrison <harrisonNatalie04@outlook.com>
2024-01-16 10:10:31 -0500

    pull

--- src/app/power-search/power-search.component.ts
+++ src/app/power-search/power-search.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatInput, MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Power } from '../power';
 import { PowerService } from '../power.service';
+import { MatOptionSelectionChange } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-power-search',
@@ -16,7 +18,7 @@ import { PowerService } from '../power.service';
 export class PowerSearchComponent {
   powers: Power[] = []; //getting the powers from the Power data
   powersForm = new FormControl([]); //when no value is selected, it's an empty array. When a value is selected it should look like powers: [{id:2, name: 'super strength}, ]
- 
+  @Output() selectionChange = new EventEmitter<MatSelectChange>
 
   constructor(
     private powerService: PowerService
@@ -26,10 +28,20 @@ export class PowerSearchComponent {
     this.getPowers();
   }
 
-  getPowers(): void{
-    this.powerService.getPowers()
-    .subscribe(powers => this.powers = powers);
+  selectionChanged(event: MatOptionSelectionChange): void {
+    //fire event to parent
   }
 
+  getPowers(): void{
+    this.powerService.getPowers()
+    .subscribe(powers => {
+      console.log(powers)
+      this.powers = powers
+    });
+  }
 
+  addNewSelect(power: Power): void{
+    const selectedPowers: Power[] = [power];
+    this.selectionChange.emit()
+  }
 }
