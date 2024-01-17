cd36dd1af27dcd646f8af300dbe7284bd99e0da5
NatalieHarrison <harrisonNatalie04@outlook.com>
2024-01-17 10:42:56 -0500

    Display hero's with specific power
    -On specific power page, the hero's that have the specific power, have their names displayed on the page

--- src/app/power-detail/power-detail.component.ts
+++ src/app/power-detail/power-detail.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Power } from '../power';
 import { PowerService } from '../power.service';
 import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-power-detail',
@@ -10,19 +12,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PowerDetailComponent {
   power: Power | undefined; 
+  heroes: Hero[] = [];
   constructor (
     private route: ActivatedRoute, 
     private powerService: PowerService,
+    private heroService: HeroService
   ) {}
 
   ngOnInit() : void{
     this.getPower();
+    this.getHeroes();
   }
 
   getPower(): void{
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.powerService.getPower(id)
     .subscribe(power => this.power = power);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
 }
