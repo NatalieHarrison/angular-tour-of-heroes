442f8a4f861913b3ec74c72f51b64b78ee19b107
NatalieHarrison <harrisonNatalie04@outlook.com>
2024-01-18 09:24:49 -0500

    component complete
    -user can select multiple powers and add to the hero
    -

--- src/app/power-select/power-select.component.ts
+++ src/app/power-select/power-select.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Power } from '../power';
+import { PowerService } from '../power.service';
+import { FormControl } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-power-select',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './power-select.component.css'
 })
 export class PowerSelectComponent {
+  $powers: Observable<Power[]> | null = null;
+  powersForm = new FormControl([]); //when no value is selected, it's an empty array. When a value is selected it should look like powers: [{id:2, name: 'super strength}, ]
 
+  @Output() selectionChangeEvent = new EventEmitter<Power[]>();
+
+
+  constructor( private powerService: PowerService){}
+
+  ngOnInit(): void {
+    this.getPowers();
+  }
+
+  getPowers(): void{
+    this.$powers = this.powerService.getPowers();
+  }
+
+  selectionChanged(event: MatSelectChange): void{
+    this.selectionChangeEvent.emit(event.value)
+  }
 }
