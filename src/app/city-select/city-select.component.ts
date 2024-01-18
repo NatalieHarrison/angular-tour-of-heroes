10fedb42f74cf47802e40c6ee739f90e7827f15e
NatalieHarrison <harrisonNatalie04@outlook.com>
2024-01-18 08:10:10 -0500

    get cities in city select comp

--- src/app/city-select/city-select.component.ts
+++ src/app/city-select/city-select.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { City } from '../city';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-city-select',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './city-select.component.css'
 })
 export class CitySelectComponent {
+  cities: City[] = [];
 
+  constructor(private cityService: CityService){}
+
+  ngOnInit(): void{
+    this.getCities();
+  }
+
+  getCities(): void {
+    this.cityService.getCities()
+      .subscribe(cities => this.cities = cities);
+
+
+  }
 }
